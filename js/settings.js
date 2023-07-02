2dcc1bb9dfec1d2a569bbd798b5bf5710732c4f7
Dmitriy <110325829+dmitriybnv@users.noreply.github.com>
2023-07-02 11:07:06 +0500

    default checkbox color override

--- js/settings.js
+++ js/settings.js
@@ -3,7 +3,9 @@
 const FIELD_WIDTH = 19;
 const FIELD_HEIGHT = 19;
 
-const DEFAULT_COLOR = '';
+// should override default browser setting
+const DEFAULT_COLOR = 'rgb(0, 96, 223)';
+
 const FOOD_COLOR = 'green';
 
 const MOVE_DELAY = 200;
